f661445d6163507cd962cb5a3ae443fee86bfb27
Zhang Qiang <qiang.z.zhang@intel.com>
2014-02-26 22:14:45 +0800

    Search a 2D Matrix (Accepted)
    Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
    
        Integers in each row are sorted from left to right.
        The first integer of each row is greater than the last integer of the previous row.
    
    For example,
    
    Consider the following matrix:
    
    [
      [1,   3,  5,  7],
      [10, 11, 16, 20],
      [23, 30, 34, 50]
    ]
    
    Given target = 3, return true.

--- matrix.cc
+++ matrix.cc
@@ -30,6 +30,55 @@ class Solution {
                 }
             }
         }
+
+        bool searchMatrix(vector<vector<int> > &matrix, int target) {
+            int m = matrix.size();
+            int n;
+
+            if (m > 0)
+                n = matrix[0].size();
+            else
+                return false;
+            if (n == 0)
+                return false;
+            int k = -1;
+            int l = 0, u = m - 1;
+            k = 0;
+            while (l <  u){
+                int mid = (l + u) / 2;
+
+                if (matrix[mid][0] > target){
+                    u = mid - 1;
+                    k = mid - 1;
+                    if (k < 0) return false;
+                } else if (matrix[mid][0] < target) {
+                    if (matrix[mid][n-1] >= target) {
+                        u = l;
+                        k = mid;
+                        break;
+                    } else {
+                        l = mid + 1;
+                        k = l;
+                        if (k >= m) return false;
+                    }
+                } else 
+                    return true;
+
+            }
+
+            l = 0; u = n -1;
+            while(l <= u){
+                int mid = (l + u) / 2;
+                if (matrix[k][mid] > target)
+                    u = mid - 1;
+                else if (matrix[k][mid] < target)
+                    l = mid + 1;
+                else 
+                    return true;
+            }
+
+            return false;
+        }
 };
 
 int main()
@@ -38,30 +87,32 @@ int main()
     vector<vector<int> > m;
     vector<int> p1, p2, p3;
     p1.push_back(1);
-    p1.push_back(2);
-    p1.push_back(3);
-    p2.push_back(4);
-    p2.push_back(5);
-    p2.push_back(6);
-    p3.push_back(7);
-    p3.push_back(8);
-    p3.push_back(9);
-
+//    p1.push_back(2);
+//    p1.push_back(3);
+    p2.push_back(3);
+//    p2.push_back(5);
+//    p2.push_back(6);
+//    p3.push_back(7);
+//    p3.push_back(8);
+//    p3.push_back(9);
+//
     m.push_back(p1);
     m.push_back(p2);
-    m.push_back(p3);
+//    m.push_back(p3);
 
     Solution S;
-    S.rotate(m);
-
-    cout <<"rotated Matrix:" <<endl;
-
-    for (int i = 0; i < 3; i++){
-        for (int j=0; j < 3; j++)
-            cout <<m[i][j];
-        cout <<endl;
-    }
-
+    //S.rotate(m);
+    if (S.searchMatrix(m, 1))
+        cout <<"Exist " <<endl;
+//
+//    cout <<"rotated Matrix:" <<endl;
+//
+//    for (int i = 0; i < 3; i++){
+//        for (int j=0; j < 3; j++)
+//            cout <<m[i][j];
+//        cout <<endl;
+//    }
+//
     cout <<endl;
 
     return 0;
