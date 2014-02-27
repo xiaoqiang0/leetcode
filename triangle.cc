894b62006b5fe1ea91ad46251418099c77b9ed2b
Zhang Qiang <qiang.z.zhang@intel.com>
2014-02-28 00:17:24 +0800

    Refine ? Failed again.

--- triangle.cc
+++ triangle.cc
@@ -35,13 +35,19 @@ class Solution {
                         break;
                     while (1) {
                         int t = v.top().j;
-                        //cout  << "POP  " << triangle[v.top().i][t]<<endl;
+                        cout  << "POP  " << triangle[v.top().i][t]<<endl;
                         sum -=  triangle[v.top().i][t];
                         v.pop();
+                        if (v.empty()) 
+                            return min;
                         int k = v.top().j;
                         if (k == t) {
-                            //cout  << "PUSH " <<triangle[v.top().i+1][v.top().j+1]<<endl;
+                            cout  << "PUSH- " <<triangle[v.top().i+1][v.top().j+1]<<endl;
                             sum += triangle[v.top().i+1][v.top().j+1];
+                            if (sum > min){
+                                sum -= triangle[v.top().i+1][v.top().j+1];
+                                continue;
+                            }
                             v.push(Point(v.top().i+1, v.top().j+1));
                             break;
                         }
@@ -49,7 +55,7 @@ class Solution {
                 } else {
                     v.push(Point(i+1, j));
                     sum += triangle[i+1][j];
-                    //cout  << "PUSH " << triangle[i+1][j] <<endl;
+                    cout  << "PUSH " << triangle[i+1][j] <<endl;
 
                 }
             }
@@ -60,11 +66,17 @@ class Solution {
 
 int main()
 {
-    vector<vector<int> > v(4, vector<int>(4));
+    vector<vector<int> > v(3, vector<int>(3));
+    v[0][0] = -1;
+    v[1][0] = 2;v[1][1] = 3;
+    v[2][0] = 1;v[2][1] = -1;v[2][2] = -3;
+
+/*
     v[0][0] = 2;
     v[1][0] = 3;v[1][1] = 4;
     v[2][0] = 6;v[2][1] = 5;v[2][2] = 7;
     v[3][0] = 4;v[3][1] = 1;v[3][2] = 8;v[3][3] = 3;
+*/
     Solution S;
     cout <<    S.minimumTotal(v) <<endl;
     return 0;
