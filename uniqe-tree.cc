ce40c9919f97f62932e7cd2b8bfad568217f292c
Zhang Qiang <qiang.z.zhang@intel.com>
2014-03-01 12:38:34 +0800

    Unique Binary Search Trees II (Accepted)
    Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
    
    For example,
    Given n = 3, your program should return all 5 unique BST's shown below.
    
       1         3     3      2      1
        \       /     /      / \      \
         3     2     1      1   3      2
        /     /       \                 \
       2     1         2                 3
    
    confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
    
    OJ's Binary Tree Serialization:
    
    The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
    
    Here's an example:
    
       1
      / \
     2   3
        /
       4
        \
         5
    
    The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".

--- uniqe-tree.cc
+++ uniqe-tree.cc
@@ -4,8 +4,37 @@
 #include <algorithm>
 
 using namespace std;
+/**
+ * Definition for binary tree
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct Node {
+    TreeNode * root;
+    TreeNode * br; //Bottom right node
+    Node(TreeNode * root, TreeNode * br) : root(root), br(br) {}
+};
 class Solution {
     public:
+        void preorder(TreeNode *root)
+        {
+            if (root == NULL) return;
+            cout <<root->val << " ";
+            preorder(root->left);
+            preorder(root->right);
+        } 
+        void inorder(TreeNode *root)
+        {
+            if (root == NULL) return;
+            inorder(root->left);
+            cout <<root->val << " ";
+            inorder(root->right);
+        }
         int numTrees(int n) {
             int sum;
             if (n == 1) return 1;
@@ -16,10 +45,103 @@ class Solution {
             for (int i = 1; i <= n-2; i++) {
                 sum += numTrees(i) * numTrees(n - 1 - i);
             }
-
             return sum;
         }
+
+        TreeNode * copy_Tree(TreeNode *root, TreeNode *old_br, TreeNode **br, int inc = 0){
+            if (root == NULL) return NULL;
+
+            TreeNode *res = new TreeNode (root->val + inc);
+
+            if (root == old_br)
+                *br = res;
+            res->left = copy_Tree(root->left, old_br, br, inc);
+            res->right = copy_Tree(root->right, old_br, br, inc);
+
+            return res;
+        }
+        vector<Node *> _generateTrees(int n) {
+            vector<Node *> res;
+
+            if (n == 0)
+                return res;
+
+            if (n == 1) {
+                TreeNode *root = new TreeNode(1);
+                res.push_back(new Node(root, root));
+                return res;
+            }
+            if (n == 2) {
+                TreeNode *root = new TreeNode(1);
+                root->right = new TreeNode(2);
+                res.push_back(new Node(root, root->right));
+                root = new TreeNode(2);
+                root->left = new TreeNode(1);
+                res.push_back(new Node(root, root));
+                return res;
+            }
+
+            vector<Node *> t = _generateTrees(n-1);
+
+            for (int i = 0; i < t.size(); i++){
+                TreeNode *br;
+                TreeNode *root = new TreeNode(n);
+
+                root->left = t[i]->root;
+                res.push_back(new Node(root, root));
+
+                root = copy_Tree(t[i]->root, t[i]->br, &br);
+
+                br->right = new TreeNode(n);
+
+                res.push_back(new Node(root, br->right));
+            }
+
+            for (int i = 1; i <= n-2; i++) {
+                TreeNode * root;
+                TreeNode * root1;
+                TreeNode * root2;
+                TreeNode *br1;
+                TreeNode *br2;
+
+                vector<Node *>M = _generateTrees(i);
+                vector<Node *>N = _generateTrees(n - 1 - i);
+
+                for (int j = 0; j < M.size(); j ++)
+                    for(int k = 0; k < N.size(); k ++){
+                        //m[i] * n[k]
+
+                        root = new TreeNode(n);
+                        root1 = copy_Tree(M[j]->root, M[j]->br, &br1);
+                        root2 = copy_Tree(N[k]->root, N[k]->br, &br2, i);
+
+                        br1->right = new TreeNode(n);
+                        br1->right->left = root2;
+
+                        res.push_back(new Node(root1, br1->right));
+                    }
+
+            }
+
+            return res;
+        } 
+
+        vector<TreeNode *> generateTrees(int n) {
+            vector<TreeNode *> res;
+
+            if (n == 0){
+                res.push_back(NULL);
+                return res;
+            }
+            vector<Node *> res1 = _generateTrees(n);
+
+            for (int i = 0; i < res1.size(); i++)
+                res.push_back(res1[i]->root);
+            return res;
+        }
+
 };
+
 
 int main()
 {
@@ -27,5 +149,17 @@ int main()
     cout << "numTrees(3):" <<S.numTrees(3)<<endl;
     cout << "numTrees(4):" <<S.numTrees(4)<<endl;
     cout << "numTrees(5):" <<S.numTrees(5)<<endl;
+    vector<TreeNode *> res = S.generateTrees(3);
+
+    cout <<"tree vector size:" << res.size() <<endl;
+    
+    for (int i = 0; i < res.size(); i++) {
+        S.preorder(res[i]);
+        cout <<endl;
+        S.inorder(res[i]);
+        cout <<endl;
+        cout <<endl;
+    }
+        
     return 0;
 }
