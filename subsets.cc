1c8c9a10de15e18eadaf233a4818deff6cb437f6
Zhang Qiang <qiang.z.zhang@intel.com>
2014-03-05 14:22:36 +0800

    Subsets II (Accepted)
    Given a collection of integers that might contain duplicates, S, return all possible subsets.
    
    Note:
    
        Elements in a subset must be in non-descending order.
        The solution set must not contain duplicate subsets.
    
    For example,
    If S = [1,2,2], a solution is:
    
    [
      [2],
      [1],
      [1,2,2],
      [2,2],
      [1,2],
      []
    ]

--- subsets.cc
+++ subsets.cc
@@ -30,15 +30,38 @@ class Solution {
             return res;
         }
 
+        void gen2(vector<int> &S, int i, vector<int> &cur,set <vector<int> > &res){
+            int n = S.size();
+            if (i == n){
+                res.insert(cur);
+            } else{
+                gen2(S, i+1, cur, res);
+                cur.push_back(S[i]);gen2(S, i+1, cur, res);
+                cur.pop_back(); 
+            }
+        }
+
+        vector<vector<int> > subsetsWithDup(vector<int> &S) {
+            vector<vector<int> > res;
+            set <vector<int> > r;
+            vector<int> cur;
+            if (S.size() == 0)
+                return res;
+            sort(S.begin(), S.end());
+            gen2(S,0, cur, r);
+            res.assign(r.begin(), r.end());
+            return res;
+        }
+
 };
 int main()
 {
-    int a[] = {4,1,0};
-    //int a[] = {1,2,2};
+    //int a[] = {4,1,0};
+    int a[] = {1,2,2};
     vector<int> v;
     v.assign(a, a + sizeof(a)/sizeof(int));
     Solution S;
-    vector<vector<int> >  res = S.subsets(v);
+    vector<vector<int> >  res = S.subsetsWithDup(v);
     for (int i = 0; i < res.size(); i ++){
         for (int j = 0; j < res[i].size(); j++)
             cout <<res[i][j] << " ";
