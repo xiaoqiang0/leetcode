78bb31b6f6d8b53a5242a599f85dbab958538ff4
Zhang Qiang <qiang.z.zhang@intel.com>
2014-03-05 14:21:24 +0800

    Subsets (Accepted)
    Given a set of distinct integers, S, return all possible subsets.
    
    Note:
    
        Elements in a subset must be in non-descending order.
        The solution set must not contain duplicate subsets.
    
    For example,
    If S = [1,2,3], a solution is:
    
    [
      [3],
      [1],
      [2],
      [1,2,3],
      [1,3],
      [2,3],
      [1,2],
      []
    ]

---
+++ subsets.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <limits>
+#include <set>
+
+using namespace std;
+class Solution {
+    public:
+        void gen(vector<int> &S, int i, vector<int> &cur, vector<vector<int> > &res){
+            int n = S.size();
+            if (i == n){
+                res.push_back(cur);
+            } else{
+                gen(S, i+1, cur, res);
+                cur.push_back(S[i]);gen(S, i+1, cur, res);
+                cur.pop_back(); 
+            }
+        }
+
+        vector<vector<int> > subsets(vector<int> &S) {
+            vector<vector<int> > res;
+            vector<int> cur;
+            if (S.size() == 0)
+                return res;
+            sort(S.begin(), S.end());
+            gen(S,0, cur, res);
+
+            return res;
+        }
+
+};
+int main()
+{
+    int a[] = {4,1,0};
+    //int a[] = {1,2,2};
+    vector<int> v;
+    v.assign(a, a + sizeof(a)/sizeof(int));
+    Solution S;
+    vector<vector<int> >  res = S.subsets(v);
+    for (int i = 0; i < res.size(); i ++){
+        for (int j = 0; j < res[i].size(); j++)
+            cout <<res[i][j] << " ";
+        cout <<endl;
+
+    }
+    return 0;
+}
