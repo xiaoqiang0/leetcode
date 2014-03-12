290bd36544ca41aaa17136551af390471f0efd6e
Zhang Qiang <qiang.z.zhang@intel.com>
2014-03-12 20:36:58 +0800

    Jump Game II (Accepted)
    Given an array of non-negative integers, you are initially positioned at the first index of the array.
    
    Each element in the array represents your maximum jump length at that position.
    
    Your goal is to reach the last index in the minimum number of jumps.
    
    For example:
    Given array A = [2,3,1,1,4]
    
    The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)

--- jump-games.cc
+++ jump-games.cc
@@ -14,7 +14,7 @@ class Solution {
 
             for (int i = 0 ; i < n;){
                 int k = A[i];
-                
+
                 if (k == 0 && i < n-1) return false;
                 i = i+k;
 
@@ -24,20 +24,56 @@ class Solution {
 
             return false;
         }
+        int compute(int A[], int i, vector<int> &v)
+        {
+            if (i == 0){
+                v[i] = 0;
+            }
+            if (v[i] != -1) return v[i];
+            int min = 1000000;
+            for (int j = i-1; j >= 0; j--) {
+                int t = compute(A, j, v);
+                if (A[j] >= i-j && min > t+1)
+                    min = t + 1;
+            }
+            v[i] = min;
+            return min;
+        }
+
+        int jump(int A[], int n) {
+            vector<int> v(n+1, 100000);
+            if (n == 0) return 0;
+            
+            v[0] = 0;
+            for (int i = 0; i < n; i++) {
+                int k = A[i];
+                if (i>0 && k <= (A[i-1] - 1))
+                    continue;
+                if (i+k >=n)
+                    k = n-i - 1;
+                while (k && (i+k) < n){
+                    if (v[i+k] > v[i] + 1)
+                        v[i+k] = v[i]+1;
+                    k--;
+                }
+            }
+
+            return v[n-1];
+        }
 };
 int main()
 {
     //int A[] = {2,3,1,1,4};
     //int A[] = {3,2,1,0,4};
-    int A[] = {2,0};
-
+    int A[] = {2,1};
     Solution S;
-
+/*
     if (S.canJump(A, sizeof(A)/sizeof(int)))
         cout << "Can jump" <<endl;
     else
         cout << "Can not jump" <<endl;
-
+*/
+    cout << "steps:" <<S.jump(A, sizeof(A)/sizeof(int))<<endl;
 
     return 0;
 }
