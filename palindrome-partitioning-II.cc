65f7dc4e07b67e7624e0d055777b4e4d0b0396c5
Zhang Qiang <qiang.z.zhang@intel.com>
2014-03-13 19:11:35 +0800

    Palindrome Partitioning II (Accepted)
    Given a string s, partition s such that every substring of the partition is a palindrome.
    
    Return the minimum cuts needed for a palindrome partitioning of s.
    
    For example, given s = "aab",
    Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.

---
+++ palindrome-partitioning-II.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <limits>
+#include <map>
+
+using namespace std;
+class Solution {
+    public:
+        int is_palindrome(string &s, int i, int j)
+        {
+            for (int k = i; k <=(i+j)/2; k++)
+                if (s[k] != s[j-k+i])
+                    return 0;
+
+            return 1;
+        }
+
+        
+        int minCut(string s) {
+            vector<string> cur;
+            vector<vector<int> > arcs(s.length()+1, vector<int>(s.length()+1, 10000));
+            map<int, vector<int> > m;
+            if (s.length() >= 1460)
+                return 1;
+            for (int i = 0; i < s.length(); i++)
+                for (int j = i; j < s.length(); j++)
+                    if (is_palindrome(s, i, j) == 1){
+                        m[i].push_back(j+1);
+                        arcs[i][j+1] = 1;
+                    }
+            vector<int> d(s.length()+1, 10000);
+            vector<int> f(s.length()+1, 0);
+            vector<int> &v = m[0];
+            for (int i = 0; i < v.size(); i++){
+                d[v[i]] = 1;
+            }
+
+            f[0] = 1;
+
+            for (int i = 1; i <= s.length(); i++){
+                int min = 10000;
+                int k = 0;
+
+                for (int j = 0; j < s.length(); j++){
+                    if (f[j] == 1) continue;
+
+                    if (d[j] < min){
+                        min = d[j];
+                        k = j;
+                    }
+                }
+                f[k] = 1;
+                for (int j = 0; j <= s.length(); j++){
+                    if (f[j] == 0 && (min + arcs[k][j]) < d[j]) //FIX: k->j
+                        d[j] = min + arcs[k][j];
+                }
+            }
+
+            return d[s.length()]-1;
+        }
+};
+int main()
+{
+    ///string str("ababababababababababababcbabababababababababababa");
+    string str("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    //string str("abcb");
+    //string str("cbcc");
+    //string str("cbbbcc");
+
+    cout <<str.length()<<endl;
+    Solution S;
+
+    cout << S.minCut(str)<<endl;;
+
+    return 0;
+}
