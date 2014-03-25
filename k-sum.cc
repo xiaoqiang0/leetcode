5d30b95f808c9d66c727d1b38474978990b41d8a
Zhang Qiang <qiang.z.zhang@intel.com>
2014-03-25 16:57:36 +0800

    3Sum (Accepted)
    Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
    
    Note:
    
        Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
        The solution set must not contain duplicate triplets.
    
        For example, given array S = {-1 0 1 2 -1 -4},
    
        A solution set is:
        (-1, 0, 1)
        (-1, -1, 2)

--- k-sum.cc
+++ k-sum.cc
@@ -5,6 +5,7 @@
 #include <limits>
 #include <queue>
 #include <map>
+#include <set>
 
 using namespace std;
 class Solution {
@@ -40,17 +41,97 @@ class Solution {
                 }
             }
         }
+
+        vector<vector<int> > threeSum1(vector<int> &num) {
+            vector<vector<int> >res;
+            set<vector<int> >r;
+
+
+            if (num.size() < 3)
+                return res;
+            sort(num.begin(), num.end());
+
+            for (int i = 0; i < num.size() - 2; i ++){
+                int j = i+1, k = num.size() - 1;
+                while (j < k) {
+                    if (num[i] + num[j] + num[k] == 0){
+                        vector<int> v;
+                        v.push_back(num[i]);
+                        v.push_back(num[j]);
+                        v.push_back(num[k]);
+                        r.insert(v);
+                        j++;k--;
+                    } else if (num[i] + num[j] + num[k] < 0)
+                        j++;
+                    else
+                        k--;
+                }
+            }
+
+            res.assign(r.begin(), r.end());
+            return res;
+        }
+
+        vector<vector<int> > threeSum(vector<int> &num) {
+            vector<vector<int> >res;
+
+            if (num.size() < 3)
+                return res;
+
+            sort(num.begin(), num.end());
+            for (int i = 0; i < num.size() - 2; i ++){
+                if (i > 0 && num[i] == num[i-1])
+                    continue;
+
+                int j = i+1, k = num.size() - 1;
+                while (j < k) {
+                    while(k>num.size()-1 && num[k]==num[k+1])
+                        k--;
+                    if (j >= k) break;
+                    while( j != i+1 && j>0 && num[j]==num[j-1])
+                        j++;
+                    if (j >= k) break;
+                    if (num[i] + num[j] + num[k] == 0){
+                        vector<int> v;
+                        v.push_back(num[i]);
+                        v.push_back(num[j]);
+                        v.push_back(num[k]);
+                        res.push_back(v);
+                        j++;k--;
+                    } else if (num[i] + num[j] + num[k] < 0)
+                        j++;
+                    else
+                        k--;
+                }
+            }
+            return res;
+        }
 };
 
 int main()
 {
-    int A[] = {0,4,0};
+    /*int A[] = {0,4,0};
+      vector<int> v;
+      v.assign(A, A+sizeof(A)/sizeof(int));
+
+      Solution S;
+      vector<int > r = S.twoSum(v, 0);
+
+      cout <<"result:" << r[0] << "," << r[1] <<endl;
+     */
+    int A[] = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
     vector<int> v;
     v.assign(A, A+sizeof(A)/sizeof(int));
 
     Solution S;
-    vector<int > r = S.twoSum(v, 0);
+    vector<vector<int> > r = S.threeSum(v);
 
-    cout <<"result:" << r[0] << "," << r[1] <<endl;
+    for (int i = 0; i < r.size(); i++){
+        for (int j = 0; j < 3; j++)
+            cout << r[i][j] << " ";
+
+        cout <<endl;
+
+    }
     return 0;
 }
