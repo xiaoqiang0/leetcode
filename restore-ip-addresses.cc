9748d4cf7b4e3be593325cfe1a68994374e48239
Zhang Qiang <qiang.z.zhang@intel.com>
2014-03-25 23:26:17 +0800

    restore IP addresses (Pending submit)

---
+++ restore-ip-addresses.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <limits>
+#include <queue>
+#include <map>
+#include <cstring>
+using namespace std;
+
+class Solution {
+    public:
+        
+        void gen(string &s, int idx, vector<string> &res, vector<string> &cur) {
+            int left = 4 - cur.size();
+            int len = s.length();
+            if (idx == len && cur.size() == 4){
+                string s;
+                s = cur[0]+"."+cur[1]+"."+cur[2]+"."+cur[3];
+
+                res.push_back(s);
+                return;
+            }
+            for (int i = idx; i < len; i++){
+                string t = s.substr(idx, i - idx + 1);
+                int n = atoi(t.c_str());
+                if (n < 0 || n > 255) break;
+                if ((len - i - 1) > 3*left) continue;
+                if ((len - i - 1) < left) break;
+                cur.push_back(s.substr(idx, i - idx + 1));
+                gen(s, i+1, res, cur);
+                cur.pop_back();
+            }
+            
+
+        }
+        vector<string> restoreIpAddresses(string s) {
+            vector<string> res;
+            vector<string> cur;
+            gen(s, 0, res, cur);
+
+            return res;
+        }
+};
+int main()
+{
+    //string s("1234573");
+    string s("25525511135");
+    Solution S;
+    vector<string> res = S.restoreIpAddresses(s);
+
+    for (int i = 0; i < res.size(); i ++)
+        cout << res[i] <<endl;
+
+    return 0;
+}
