25ef13aa6e72bb77666f063d34d816eaeb4fcb32
Zhang Qiang <qiang.z.zhang@intel.com>
2014-04-02 13:41:24 +0800

    Wildcard Matching (Accepted)
    Implement wildcard pattern matching with support for '?' and '*'.
    
    '?' Matches any single character.
    '*' Matches any sequence of characters (including the empty sequence).
    
    The matching should cover the entire input string (not partial).
    
    The function prototype should be:
    bool isMatch(const char *s, const char *p)
    
    Some examples:
    isMatch("aa","a") → false
    isMatch("aa","aa") → true
    isMatch("aaa","aa") → false
    isMatch("aa", "*") → true
    isMatch("aa", "a*") → true
    isMatch("ab", "?*") → true
    isMatch("aab", "c*a*b") → false

--- wildcard-matching.cc
+++ wildcard-matching.cc
@@ -9,78 +9,167 @@
 using namespace std;
 class Solution {
     public:
-        bool eval(string &s, int i, string &p, int j, vector<vector<int> >&v) {
-            bool res;
-            if (s.length() < i || p.length() < j) return false;
-            if (i == s.length() && j == p.length()) return true;
-            //p is not start with x*
-            if (!(j < p.length() && p[j] == '*'))
-                if (!(i < s.length() && j < p.length() )) {
-                    return false;
-                }
-            
-            if (i == s.length() && p.find_first_not_of("*?", j) != string::npos){
-                res = false;
-                if (res == false)
-                    v[i][j] = 0;
-                else
-                    v[i][j] = 1;
-                return res;
-            }
-            if (v[i][j] != -1)
-                return v[i][j] == 1?true:false;
-            while (j < p.length()-1 && p[j] == '*' && p[j+1] == '*')
-                j++;
-            if (p[j] == '?') {
-                res = (i < s.length() && eval(s, i+1,p, j+1, v));
-                if (res == false)
-                    v[i][j] = 0;
-                else
-                    v[i][j] = 1;
-                return res;
-            }
-            
-            if (p[j] == '*'){
-                int k = p.find_first_of("*?", j+1);
-                if (k == j+1) { //*?....
-                    res = (i < s.length() && eval(s, i+1,p, j, v)) || eval(s, i,p, j+1, v);
-                    if (res == false)
-                        v[i][j] = 0;
-                    else
-                        v[i][j] = 1;
+        //find all matched pos 
+        vector<int> find_all(string &s, string &p, int pre){
+            vector<int> res;
+            for (int i = pre; i < s.length();){
+                int cur = s.find(p, i);
+                if (cur == string::npos)
                     return res;
-                }
-                string sub = p.substr(j+1, k-j-1);
-                int found = i;
-                bool res = false;
-                found = s.find(sub, found);
-                if (found == string::npos) {
-                    v[i][j] = 0;
-                    return false;
-                }
-                res = eval(s, found+sub.length(), p, j+sub.length()+1, v) || eval(s, found+1, p, j, v);
-                if (res == true) {
-                    v[i][j] = 1;
-                    return true;
-                }
-
-            } else {
-                res = (s[i] == p[j] && eval(s, i+1, p, j+1, v));
-                if (res == false)
-                    v[i][j] = 0;
-                else
-                    v[i][j] = 1;
-                return res;
+                res.push_back(cur);
+                i = cur+1;
             }
-        
+
+            return res;
         }
+
+        bool check(int N, int pos, vector<pair<string, vector<int> > > &v, int i, vector<vector<int> >  &C){
+            if (C[i][pos] != -1) return (C[i][pos] == 1? true:false) ;
+
+            if (i == v.size() && pos == N) {
+                C[i][pos] = 1;
+                return true;
+            }
+
+            //v end or [ pos at end and v[i] is not *]
+            if (i == v.size() || (pos == N && v[i].first[0] != '*')){
+                C[i][pos] = 0;
+                return false;
+            }
+
+            string ns = v[i].first;
+            vector<int> &nv = v[i].second;
+
+            int skip = 0;
+            bool res;
+            bool have_star = false;
+            int k = i; //var for finding first sub string in pp
+
+            switch(ns[0]) {
+                case '?':
+                    res = check(N, pos + nv[0], v, i+1, C);
+                    break;
+                case '*':
+                    have_star = true;
+                    if (i == v.size()-1){
+                        res = true;
+                        break;
+                    } else {
+                        k++;
+                        ns = v[k].first;
+                        nv = v[k].second;
+                        //Try to find a substr, then go to default lable.
+                        while (ns[0] == '?' || ns[0] == '*'){
+                            if (ns[0] == '?') {
+                                skip += nv[0];
+                                if (skip + pos > N){
+                                    res = false;
+                                    C[i][pos] = 0;
+                                    return res;
+                                }
+                            }
+                            k++;
+                            if (k == v.size())
+                                break;
+                            ns = v[k].first;
+                            nv = v[k].second;
+                        }
+
+                        if ((ns[0] == '?' || ns[0] == '*')){
+                            // no enough chars  for '?'
+                            if (skip > N - pos) {
+                                res = false;
+                                C[i][pos] = 0;
+                            } else { // * found, so it always true
+                                res = true;
+                                C[i][pos] = 1;
+                            }
+
+                            return res;
+                        }
+                    }
+                default:
+                    // iterate all match position
+                    for (int j = 0; j < nv.size(); j++){
+                        int cur = nv[j];
+                        if (cur < pos + skip) continue;
+
+                        if (have_star == false && cur != pos) {
+                            res = false;
+                            break;
+                        }
+
+                        res = check(N, cur + ns.length(), v, k+1, C);
+                        if (res == true){
+                            res = true;
+                            break;
+                        }
+
+                        // can't continue to check next if no star (*)
+                        if (have_star == false){
+                            res = false;
+                            break;
+                        }
+                        nv[j] = numeric_limits<int>::min();
+                    }
+
+                    // Clear invalid match position
+                    while (nv.empty() == false && nv[0] == numeric_limits<int>::min())
+                        nv.erase(nv.begin());
+            }
+
+            C[i][pos] = (res == true ? 1: 0);
+            return res;
+        }
+
         bool isMatch(const char *s, const char *p) {
             string ss(s);
             string pp(p);
-            //if (ss.length() >= 200)
-            //    return false;
-            vector<vector<int> >v(ss.length()+1, vector<int>(pp.length()+1, -1));
-            return eval(ss, 0, pp, 0, v);
+            vector<pair<string, vector<int> > > v;
+
+            //matched previous position in s
+            int pre = 0;
+
+            for (int i = 0; i < pp.size();){
+                int cur;
+                switch (pp[i]) {
+                    case '*':
+                        cur = pp.find_first_not_of('*', i);
+                        v.push_back(make_pair("*", vector<int>(1, -1)));
+                        i  = cur;
+                        break;
+
+                    case '?':
+                        cur = pp.find_first_not_of('?', i);
+                        if (cur == string::npos)
+                            cur = pp.length();
+                        v.push_back(make_pair("?", vector<int>(1, cur-i)));
+                        pre += cur-i;
+                        //No enough chars for '?'s, return false directly.
+                        if (pre > ss.length())
+                            return false;
+                        i  = cur;
+                        break;
+                    default:
+                        cur = pp.find_first_of("*?", i);
+                        string sub(pp.substr(i, cur-i));
+                        vector<int> vv = find_all(ss, sub, pre);
+
+                        // No sub string in ss can match 'sub', so return false directly
+                        if (vv.size() == 0)
+                            return  false;
+
+                        // Create pair
+                        v.push_back(make_pair(sub, vv));
+                        i = cur;
+                        // Increase pre.
+                        pre = vv[0] + sub.length();
+                }
+            }
+
+            vector<vector<int> >  C(v.size()+1, vector<int>(ss.length()+1, -1));
+
+            return check(ss.length(), 0, v, 0, C);
         }
 };
 int main()
@@ -102,15 +191,27 @@ int main()
     char *p = "a";
     char *s = "aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba";
     char *p = "a*******b";
-    char *s = "abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb";
-    char *p = "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb";
     char *s = "c";
     char *p = "*?*";
-*/
     char *s = "aaaabaabaabbbabaabaabbbbaabaaabaaabbabbbaaabbbbbbabababbaabbabbbbaababaaabbbababbbaabbbaabbaaabbbaabbbbbaaaabaaabaabbabbbaabababbaabbbabababbaabaaababbbbbabaababbbabbabaaaaaababbbbaabbbbaaababbbbaabbbbb";
     char *p = "**a*b*b**b*b****bb******b***babaab*ba*a*aaa***baa****b***bbbb*bbaa*a***a*a*****a*b*a*a**ba***aa*a**a*";
+    char *s = "abbbaaababbaaabaaabbbabbbbaaabbaaababaabbbbbbaababbabababbababaaabbbbbabababaababaaaaaaabbbaabaabbbaabbabaababbabaababbbabbaaabbbaaaababbaaabbaabaabbbbbaaababaabaabaaabbabaabbbabbbaabbababaabbbbbbbbaaa";
+    char *p = "*ba***bba*b**abbaa***a*****b*a*bb*b***a*bbb***a***bba*****a****a*a*b**aaaba*aab*a*aa***a*a*b**b**a*b*";
+    char *s = "xyzabcxabc";
+    char *p = "xy*?abc";
+    char *s = "abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb";
+    char *p =   "*aa*ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb";
+    char *s = "baaaba";
+    char *p = "?*?***";
+    char *s = "abbaabbbbababaababababbabbbaaaabbbbaaabbbabaabbbbbabbbbabbabbaaabaaaabbbbbbaaabbabbbbababbbaaabbabbabb";
+    char *p = "***b**a*a*b***b*a*b*bbb**baa*bba**b**bb***b*a*aab*a**";
+    char *s = "abbaabbbbababaababababbabbbaaaabbbbaaabbbabaabbbbbabbbbabbabbaaabaaaabbbbbbaaabbabbbbababbbaaabbabbabb";
+    char *p = "***b**a*a*b***b*a*b*bbb**baa*bba**b**bb***b*a*aab*a**";
+*/
+    string s("ab");
+    string p("*a");
     Solution S;
-    cout << S.isMatch(s, p) <<endl;
+    cout << S.isMatch(s.c_str(), p.c_str()) <<endl;
 
     return 0;
 }
