6597eb0d86b1c45b5415bbcc206ab6c4aa04e45c
Zhang Qiang <qiang.z.zhang@intel.com>
2014-04-18 21:28:19 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+#include <map>
+#include <unordered_set>
 using namespace std;
 
 int main()
@@ -24,5 +26,15 @@ int main()
     string s;
     s.append(to_string (12));
     cout <<s;
+    //--------------------------------
+    unordered_set<std::string> A( {"log", "dog", "hot", "hit"});
+    unordered_set<std::string> B( {"hit", "hot"});
+    unordered_set<std::string> C;
+
+    set_intersection(A.begin(), A.end(), B.begin(), B.end(), std::insert_iterator< std::unordered_set<string> >(C,C.begin()));
+
+    for (auto it = C.begin(); it != C.end(); it++)
+        cout <<*it <<endl;
+
     //std::find(res.begin(), res.end(), 1)!=res.end();
 }
